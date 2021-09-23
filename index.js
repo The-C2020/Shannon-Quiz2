139faf90955e1f3083bc3bd2d7f1b9a416958a12
The-C2020 <chris11235@hotmail.com>
2021-09-23 20:46:00 +0200

    second commit

---
+++ index.js
@@ -0,0 +1,59 @@
+function getDraggables() {
+  const draggables = document.querySelectorAll(".draggable");
+  const sortContainer = document.querySelector(".sortables");
+  draggables.forEach((draggable) => {
+    draggable.addEventListener("dragstart", () => {
+      draggable.classList.add("dragging");
+    });
+
+    draggable.addEventListener("dragend", () => {
+      draggable.classList.remove("dragging");
+    });
+  });
+
+  sortContainer.addEventListener("dragover", (e) => {
+    e.preventDefault();
+    const afterElement = getDragAfterElement(sortContainer, e.clientY);
+    const draggable = document.querySelector(".dragging");
+    console.log(afterElement);
+    if (afterElement == null) {
+      sortContainer.appendChild(draggable);
+    } else {
+      sortContainer.insertBefore(draggable, afterElement);
+    }
+  });
+
+  function getDragAfterElement(container, y) {
+    const draggableElements = [
+      ...container.querySelectorAll(".draggable:not(.dragging)"),
+    ];
+
+    return draggableElements.reduce(
+      (closest, child) => {
+        const box = child.getBoundingClientRect();
+        const offset = y - box.top - box.height / 2;
+        if (offset < 0 && offset > closest.offset) {
+          return { offset: offset, element: child };
+        } else {
+          return closest;
+        }
+      },
+      { offset: Number.NEGATIVE_INFINITY }
+    ).element;
+  }
+}
+
+getDraggables();
+
+// sortContainers2.forEach((container) => {
+//   container.addEventListener("dragover", (e) => {
+//     e.preventDefault();
+//     const afterElement = getDragAfterElement(container, e.clientY);
+//     const draggable = document.querySelector(".dragging");
+//     if (afterElement == null) {
+//       container.appendChild(draggable);
+//     } else {
+//       container.insertBefore(draggable, afterElement);
+//     }
+//   });
+// });
